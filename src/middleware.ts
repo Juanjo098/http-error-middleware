87802f2f10376b6ae1f4903482d2beac7cf05bb6
Juanjo098 <juanjo.silva.098@gmail.com>
2024-12-25 13:44:29 -0600

    feat(middleware.ts): :sparkles: Create middleware manage http errors throwed by HttpError class

---
+++ src/middleware.ts
@@ -0,0 +1,12 @@
+import { NextFunction, Request, Response } from 'express'
+
+import { HttpError } from './HttpError'
+
+export function httpErrorMiddleware(err: any, _req: Request, res: Response, next: NextFunction) {
+  if (err instanceof HttpError) {
+    const { message, statusCode, details } = err
+    res.status(statusCode).json({ message, details })
+  } else {
+    next()
+  }
+}
