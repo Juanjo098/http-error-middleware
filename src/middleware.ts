550c748781fd51f8d70392c96f62b04244310465
Juanjo098 <juanjo.silva.098@gmail.com>
2024-12-27 12:18:05 -0600

    feat(Middleware): :sparkles: Implement new option "statusCodeOnResponse" to send or not response status code on response body

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,15 +3,29 @@ import { NextFunction, Request, Response } from 'express'
 import { HttpError } from '@/HttpError'
 
 interface HttpErrorMiddlewareProps {
-  destructure: boolean
+  destructure: boolean,
+  statusCodeOnResponse: boolean
 }
 
 export function httpErrorMiddleware(options: Partial<HttpErrorMiddlewareProps> = {}) {
   return function (err: any, _req: Request, res: Response, next: NextFunction) {
     if (err instanceof HttpError) {
+      const { destructure, statusCodeOnResponse } = options
       const { message, statusCode, details } = err
-      if (options.destructure) res.status(statusCode).json({ message, statusCode, ...details })
-      else res.status(statusCode).json({ message, statusCode, details })
+
+      let allData: any = { message, statusCode, details }
+
+      if (statusCodeOnResponse !== undefined && !statusCodeOnResponse) {
+        const { statusCode:_, ...restData } = allData
+        allData = restData
+      } 
+
+      if (destructure) {
+        const { details: destructureDetails, ...restData } = allData
+        allData = { ...restData, ...destructureDetails }
+      } 
+
+      res.status(statusCode).json({ ...allData })
     } else {
       next(err)
     }
