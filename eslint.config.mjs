a2b1eac411ebf4d1b899922516725e5bd977fe70
Juanjo098 <juanjo.silva.098@gmail.com>
2024-12-27 09:20:44 -0600

    chore(eslint.config.mjs): :wrench: Update eslint rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -34,7 +34,14 @@ export default [
       'no-extra-semi': 'error', 
       'comma-dangle': ['warn', 'never'],
       '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn'
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          'argsIgnorePattern': '^_',
+          'varsIgnorePattern': '^_',
+          'caughtErrorsIgnorePattern': '^_'
+        }
+      ]
     }
   },
   {
